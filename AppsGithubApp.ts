6bbef1c514b1bce2f65b1b2a5d476d6fd4b4d2f1
Douglas Gubert <douglas.gubert@gmail.com>
2019-05-08 11:54:39 -0300

    First iteration

--- AppsGithubApp.ts
+++ AppsGithubApp.ts
@@ -1,12 +1,24 @@
 import {
     IAppAccessors,
+    IConfigurationExtend,
+    IEnvironmentRead,
     ILogger,
 } from '@rocket.chat/apps-engine/definition/accessors';
+import { ApiSecurity, ApiVisibility } from '@rocket.chat/apps-engine/definition/api';
 import { App } from '@rocket.chat/apps-engine/definition/App';
 import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { WebhookEndpoint } from './endpoints/webhook';
 
 export class AppsGithubApp extends App {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
+    }
+
+    protected async extendConfiguration(configuration: IConfigurationExtend, environmentRead: IEnvironmentRead): Promise<void> {
+        configuration.api.provideApi({
+            visibility: ApiVisibility.PUBLIC,
+            security: ApiSecurity.UNSECURE,
+            endpoints: [new WebhookEndpoint(this)],
+        });
     }
 }
---
+++ endpoints/webhook.ts
@@ -0,0 +1,45 @@
+import { IHttp, IModify, IPersistence, IRead } from '@rocket.chat/apps-engine/definition/accessors';
+import { ApiEndpoint, IApiEndpointInfo, IApiRequest, IApiResponse } from '@rocket.chat/apps-engine/definition/api';
+import { IRoom } from '@rocket.chat/apps-engine/definition/rooms';
+
+export class WebhookEndpoint extends ApiEndpoint {
+    public path = 'webhook';
+
+    public async post(
+        request: IApiRequest,
+        endpoint: IApiEndpointInfo,
+        read: IRead,
+        modify: IModify,
+        http: IHttp,
+        persis: IPersistence,
+    ): Promise<IApiResponse> {
+        const sender = await read.getUserReader().getById('rocket.cat');
+        const room = await read.getRoomReader().getById('GENERAL') || {} as IRoom;
+
+        if (request.headers['x-github-event'] !== 'push') {
+            return this.success();
+        }
+
+        let payload: any;
+
+        if (request.headers['content-type'] === 'application/x-www-form-urlencoded') {
+            payload = JSON.parse(request.content.payload);
+        } else {
+            payload = request.content;
+        }
+
+        const message = modify.getCreator().startMessage({
+            room,
+            sender,
+            avatarUrl: payload.sender.avatar_url,
+            alias: payload.sender.login,
+            text: `[${payload.sender.login}](${payload.sender.html_url}) just pushed ${
+                payload.commits.length
+            } commits to [${payload.repository.full_name}](${payload.repository.html_url})`,
+        });
+
+        modify.getCreator().finish(message);
+
+        return this.success();
+    }
+}
